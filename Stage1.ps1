2a194078d4bef268b8f7bd777f450f4b9f90ffac
nohour <vadim@khaikin.biz>
2018-04-09 11:32:15 +0300

    Add files via upload

--- Stage1.ps1
+++ Stage1.ps1
@@ -5,5 +5,5 @@ $json = Get-Content -Path "$PSScriptRoot\params.json"
 $hashtable = $json| ConvertFrom-Json
 $param = $hashtable.segments | Where {$_.segment -like $segment}
 
-
-Write-Host "Running test run for Env {0} IP segment is {1} SubnetMaskIp for all is {2}" -f $param.segment, $param.ipSegment, $hashtable.CommonNetParams.SubnetMaskIp
+$output = "Running test run for Env {0} IP segment is {1} SubnetMaskIp for all is {2}" -f $param.segment, $param.ipSegment, $hashtable.CommonNetParams.SubnetMaskIp
+Write-Host $output
